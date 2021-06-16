caf1ba5aebb39c0db66c7afd48e0e4a387f126a1
alexwu7583 <alex@achievr.io>
2021-06-16 15:05:21 +1000

    setup react native web

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 // dependencies
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+// import {NavigationContainer} from '@react-navigation/native';
 // components
-import BottomTabsNavigation from './src/navigation/BottomTabsNavigation';
+// import BottomTabsNavigation from './src/navigation/BottomTabsNavigation';
+
+import {View, Text} from 'react-native';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <BottomTabsNavigation></BottomTabsNavigation>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <BottomTabsNavigation></BottomTabsNavigation>
+    // </NavigationContainer>
+    <View>
+      <Text>hello world</Text>
+    </View>
   );
 };
 
